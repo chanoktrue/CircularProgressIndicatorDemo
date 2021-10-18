ee2f738754011b967a9cf3b859fc0c52b388eb2c
Thongchai Subsaidee <chanoktrue@me.com>
2021-10-18 09:32:04 +0700

    Initial commit

---
+++ app/src/main/java/com/trueddns/homenano/circularprogressindicatordemo/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.trueddns.homenano.circularprogressindicatordemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.trueddns.homenano.circularprogressindicatordemo.ui.theme.CircularProgressIndicatorDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HomeView()
+        }
+    }
+}
+
+
+@Composable
+fun HomeView() {
+
+    var progress by remember {
+        mutableStateOf(0.1f)
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        CircularProgressIndicator()
+
+        Spacer(
+            modifier = Modifier
+                .height(50.dp)
+
+        )
+        
+        LinearProgressIndicator()
+    }
+}
+
+@Composable
+@Preview
+fun HomeViewPreview() {
+    HomeView()
+}
